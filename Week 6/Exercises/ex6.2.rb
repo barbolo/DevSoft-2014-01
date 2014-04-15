0878226347dcda193886674222c0ef7c52d42146
Débora Setton <debora.setton@infosimples.com.br>
2014-04-15 20:05:38 -0300

    Updates ex6.2.rb

--- Week 6/Exercises/ex6.2.rb
+++ Week 6/Exercises/ex6.2.rb
@@ -1,2 +1,14 @@
+# Este arquivo é executado corretamente na sua máquina virtual (Vagrant)?
+# E no servidor remoto da Infosimples?
+
+# Se, em algum dos casos, o arquivo não puder ser executado, qual é o problema?
+# Corrija o problema alterando este arquivo, se for o caso.
+
+# Para executar no servidor remoto, executar os seguintes comandos:
+# $ ssh {username}@server.devsoft.infosimples.com
+# $ ruby /home/shared/ex6.2.rb
+
 puts 'Olá, mundo!'
-puts 'O encoding padrão de arquivos em Ruby é _______!'
+puts "O encoding padrão de arquivos em Ruby (versão #{RUBY_VERSION}) é _______!"
+
+# Qual é o encoding padrão em cada versão do Ruby?
