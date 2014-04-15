353ea93a7e8d61be4c7aada5cddb4a0b98d0ce4d
Débora Setton <debora.setton@infosimples.com.br>
2014-04-15 19:03:57 -0300

    Adds ex6.3, 6.4 and updates slides.

--- Week 6/Exercises/ex6.3.rb
+++ Week 6/Exercises/ex6.3.rb
@@ -1,10 +1,115 @@
+# encoding: utf-8
+
 # -----------------
 # Introdução
 # -----------------
 
+# O objetivo deste exercício é avaliar como o desempenho de um determinado
+# programa ou trecho de código varia de acordo com o número de threads
+# utilizadas.
 
+# Vamos avaliar o uso de threads em dois tipos de programa: o primeiro é do tipo
+# "IO-intensive", isto é, um programa que gasta a maior parte do tempo em
+# execuções de I/O (entrada e saída), como fazer o download de arquivos da
+# Internet.
+
+# O segundo programa é do tipo "CPU-intensive", isto é, um programa que gasta a
+# maior parte do tempo fazendo cálculos (utilizando o processador).
+
+# Para cada um destes programas, você deverá escrever três versões dele:
+# 1) Uma versão sequencial (que não usa threads)
+# 2) Uma versão que usa um número pequeno de threads
+# 3) Uma versão que usa um número muito grande de threads
+
+# A biblioteca de benchmark do Ruby
+# (http://ruby-doc.org/stdlib-1.9.2/libdoc/benchmark/rdoc/Benchmark.html)
+# será utilizada para comparar o desempenho destas diferentes versões.
 
 # -----------------
-# Exercício 6.3
+# Exercício 6.4
 # -----------------
 
+# Importa a biblioteca de benchmark do Ruby
+require 'benchmark'
+require 'mechanize'
+
+# Cada versão será executada N vezes, para garantir condições médias de
+# desempenho.
+N = 50_000
+
+def links
+
+  # Esta função baixa todos os links desta página:
+  # http://en.wikipedia.org/wiki/List_of_programming_languages
+
+  # Vamos utilizar uma biblioteca chamada Mechanize para fazer download
+  # dos links.
+
+  m = Mechanize.new
+
+  # Baixa a página principal, que tem todos os links.
+  m.get('http://en.wikipedia.org/wiki/List_of_programming_languages')
+
+  links = m.page.links.
+    map { |link| link.href }.
+    select { |path| path =~ /\A\/wiki/ }.
+    map { |path| "http://en.wikipedia.org/#{path}" }
+
+end
+
+def io_v1
+  # Versão sequencial do programa IO-intensive.
+  # Escreva aqui uma função que itera sobre os links retornados pela
+  # função links e usa o Mechanize para baixar cada link.
+  # Utilize uma nova instância do Mechanize por iteração.
+end
+
+def io_v2
+  # Versão do IO-intensive com 10 threads.
+end
+
+def io_v3
+  # Versão do IO-intensive com 100 threads.
+end
+
+def sum(n)
+  500_000.downto(n).reduce(:+)
+end
+
+def cpu_v1
+  # Versão sequencial do programa CPU-intensive.
+  # Escreva uma função que chama a função sum(n) para todos os inteiros
+  # entre 0 e 100.
+end
+
+def cpu_v2
+  # Versão do CPU-intensive com 10 threads.
+end
+
+def cpu_v3
+  # Versão do CPU-intensive com 100 threads.
+end
+
+puts "Execução dos programas 'IO-intensive' (download de arquivos):"
+puts "-------------------------------------------------------------\n\n"
+
+Benchmark.bm do |reporter|
+  reporter.report { io_v1 }
+  reporter.report { io_v2 }
+  reporter.report { io_v3 }
+end
+
+puts "\n\n"
+puts "Execução dos programas 'CPU-intensive (cálculo de fatorial)':"
+puts "-------------------------------------------------------------\n\n"
+
+Benchmark.bm do |reporter|
+  reporter.report { cpu_v1 }
+  reporter.report { cpu_v2 }
+  reporter.report { cpu_v3 }
+end
+
+#
+# Escreva as suas conclusões sobre como o desempenho variou para cada versão
+# (melhorou ou piorou?) e para cada tipo de programa.
+#
--- Week 6/Exercises/ex6.4.rb
+++ Week 6/Exercises/ex6.4.rb
@@ -4,112 +4,116 @@
 # Introdução
 # -----------------
 
-# O objetivo deste exercício é avaliar como o desempenho de um determinado
-# programa ou trecho de código varia de acordo com o número de threads
-# utilizadas.
+# A linguagem Ruby possui diversos métodos e operadores que são usados,
+# normalmente, com expressões regulares.
+# Alguns dos mais usados são: scan, match, gsub e sub.
 
-# Vamos avaliar o uso de threads em dois tipos de programa: o primeiro é do tipo
-# "IO-intensive", isto é, um programa que gasta a maior parte do tempo em
-# execuções de I/O (entrada e saída), como fazer o download de arquivos da
-# Internet.
+# Estes métodos estão descritos de forma simplificada a seguir.
+# A documentação completa pode ser encontrada em:
+# http://ruby-doc.org/core-2.0/String.html
 
-# O segundo programa é do tipo "CPU-intensive", isto é, um programa que gasta a
-# maior parte do tempo fazendo cálculos (utilizando o processador).
+# 1. SCAN
+# -------
 
-# Para cada um destes programas, você deverá escrever três versões dele:
-# 1) Uma versão sequencial (que não usa threads)
-# 2) Uma versão que usa um número pequeno de threads
-# 3) Uma versão que usa um número muito grande de threads
+# O método `scan` recebe uma regex como parâmetro e retorna um array com todos
+# os trechos da string que "deram match" com o padrão fornecido.
 
-# A biblioteca de benchmark do Ruby
-# (http://ruby-doc.org/stdlib-1.9.2/libdoc/benchmark/rdoc/Benchmark.html)
-# será utilizada para comparar o desempenho destas diferentes versões.
+# Exemplo:
+"Banana".scan(/na/) # => ["na", "na"]
+
+# 2. MATCH
+# -------
+
+# O método `match` recebe uma regex e retorna um objeto que pode ser usado para
+# acessar diversos atributos do primeiro "match". Note que, ao contrário do
+# método `scan`, o método `match` só encontra uma ocorrência do padrão, e não
+# todas.
+
+# Exemplo:
+"Banana".match(/ba/) # => nil (sem match)
+
+# Captura o DDD e o restante de um número de telefone:
+md = "11 9999-9999".match(/\A(\d\d) ([0-9\-]+)/)
+
+# Imprime os grupos capturados:
+puts "DDD is: #{md[1]}"
+puts "Number is: #{md[2]}"
+
+# 3. GSUB
+# -------
+
+# O método gsub recebe dois parâmetros: um padrão de match (regex) e um padrão
+# de substituição, e substitui todas as ocorrências do padrão pelo padrão de
+# substituição:
+
+"Hello, World!!".gsub(/[^a-z]/, "*") # => "*ello***orld**"
+
+# Se a substituição for mais complexa, é possível passar um bloco para o `gsub`
+# ao invés de uma string no segundo parâmetro e, neste bloco, você pode usar
+# qualquer expressão Ruby. Exemplo:
+
+"Hello, World!!".gsub(/[^a-z]/) do |match|
+  # Só substituir se o match for um ponto de exclamação:
+  if match == '!'
+    '*'
+  else
+    match
+  end
+end # => "Hello, World**"
+
+# 3. SUB
+# -------
+
+# O método sub funciona da mesma forma que o gsub, mas realiza a substituição
+# apenas na primeira ocorrência do padrão:
+
+"Hello, World!!".sub(/[^a-z]/, "*") # => "*ello, World!!"
 
 # -----------------
-# Exercício 6.4
+# Exercício 6.3
 # -----------------
 
-# Importa a biblioteca de benchmark do Ruby
-require 'benchmark'
-require 'mechanize'
+# Com base nas notícias abaixo, escreva expressões regulares para responder
+# às perguntas abaixo.
 
-# Cada versão será executada N vezes, para garantir condições médias de
-# desempenho.
-N = 50_000
+str = <<-STR
+09:55 - [política] Prefeito da Krakosia é eleito o melhor para economia
+10:14 - [cotidiano] Cachorro salva dono de assalto
+11:01 - [esporte] Seleção local de golfe se classifica para mundial
+11:36 - [esporte] João da Silva Sauro é cortado da seleção de golfe
+13:19 - [economia] Desemprego sobe para 12% no último mês
+14:55 - [política] Lei de incentivo ao esporte é aprovada no senado
+16:13 - [economia] Cotação do dólar sobe 5% em uma semana
+19:26 - [esporte] Liga nacional de basquete tem recorde de público
+STR
 
-def links
+# 1) Capturar o horário, tema, e título de cada notícia. Em seguida, para cada
+# notícia, imprimi-la com o seguinte formato: "TEMA: TÍTULO (HORA)"
 
-  # Esta função baixa todos os links desta página:
-  # http://en.wikipedia.org/wiki/List_of_programming_languages
+# Seu código aqui...
 
-  # Vamos utilizar uma biblioteca chamada Mechanize para fazer download
-  # dos links.
+# 2) Quantas linhas existem nesta string?
 
-  m = Mechanize.new
+# Seu código aqui...
+puts "Existem _____ linhas."
 
-  # Baixa a página principal, que tem todos os links.
-  m.get('http://en.wikipedia.org/wiki/List_of_programming_languages')
+# 3) Quantas notícias de economia de falam sobre o dólar?
 
-  links = m.page.links.
-    map { |link| link.href }.
-    select { |path| path =~ /\A\/wiki/ }.
-    map { |path| "http://en.wikipedia.org/#{path}" }
+# Seu código aqui...
+puts "_____ notícias falam sobre o dólar."
 
-end
+# 4) Imprimir todos os horários das notícias que contém a palavra golfe.
 
-def io_v1
-  # Versão sequencial do programa IO-intensive.
-  # Escreva aqui uma função que itera sobre os links retornados pela
-  # função links e usa o Mechanize para baixar cada link.
-  # Utilize uma nova instância do Mechanize por iteração.
-end
+# 5) Substituir todos os temas de notícias pela sua versão em maiúsculas e
+# imprimir a string resultante. Ou seja: seu código deverá imprimir o seguinte:
 
-def io_v2
-  # Versão do IO-intensive com 10 threads.
-end
+# 09:55 - [POLíTICA] Prefeito da Krakosia é eleito o melhor para economia
+# 10:14 - [COTIDIANO] Cachorro salva dono de assalto
+# 11:01 - [ESPORTE] Seleção local de golfe se classifica para mundial
+# 11:36 - [ESPORTE] João da Silva Sauro é cortado da seleção de golfe
+# 13:19 - [ECONOMIA] Desemprego sobe para 12% no último mês
+# 14:55 - [POLíTICA] Lei de incentivo ao esporte é aprovada no senado
+# 16:13 - [ECONOMIA] Cotação do dólar sobe 5% em uma semana
+# 19:26 - [ESPORTE] Liga nacional de basquete tem recorde de público
 
-def io_v3
-  # Versão do IO-intensive com 100 threads.
-end
-
-def sum(n)
-  500_000.downto(n).reduce(:+)
-end
-
-def cpu_v1
-  # Versão sequencial do programa CPU-intensive.
-  # Escreva uma função que chama a função sum(n) para todos os inteiros
-  # entre 0 e 100.
-end
-
-def cpu_v2
-  # Versão do CPU-intensive com 10 threads.
-end
-
-def cpu_v3
-  # Versão do CPU-intensive com 100 threads.
-end
-
-puts "Execução dos programas 'IO-intensive' (download de arquivos):"
-puts "-------------------------------------------------------------\n\n"
-
-Benchmark.bm do |reporter|
-  reporter.report { io_v1 }
-  reporter.report { io_v2 }
-  reporter.report { io_v3 }
-end
-
-puts "\n\n"
-puts "Execução dos programas 'CPU-intensive (cálculo de fatorial)':"
-puts "-------------------------------------------------------------\n\n"
-
-Benchmark.bm do |reporter|
-  reporter.report { cpu_v1 }
-  reporter.report { cpu_v2 }
-  reporter.report { cpu_v3 }
-end
-
-#
-# Escreva as suas conclusões sobre como o desempenho variou para cada versão
-# (melhorou ou piorou?) e para cada tipo de programa.
-#
+# Seu código aqui...
